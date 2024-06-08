17f1b2608ad0deaeadf53f67047ba1aaf749c2ea
Amr Elskhawy <amrelskhawy@gmail.com>
2024-06-08 22:20:24 +0300

    generate header component

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'restoX-header',
+  templateUrl: './header.component.html',
+  styles: '',
+  standalone: true
+})
+export class HeaderComponent { }
