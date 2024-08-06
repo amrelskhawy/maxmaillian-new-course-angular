5e26c4e0be71b1604e358c8cc31dd4c04a9736b4
Amr Elskhawy <amrelskhawy@gmail.com>
2024-08-06 21:52:07 +0300

    Add Comments in user card

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -11,11 +11,14 @@ const randomIndex = Math.floor( Math.random() * DUMMY_USERS.length )
   styleUrl: './user.component.scss'
 })
 export class UserComponent {
+
+  // Intialize random selected user
   selectedUser = signal(DUMMY_USERS[randomIndex])
 
-
+  // generate function compute the path of the user avatar
   getImagePath = computed(() => 'assets/users/' + this.selectedUser().avatar)
 
+  // On Click user card func
   onSelectUser() {
     const randomIndex = Math.floor( Math.random() * DUMMY_USERS.length )
     this.selectedUser.set(DUMMY_USERS[randomIndex])
